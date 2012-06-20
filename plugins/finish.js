1c780463ecee3ac3584ed50933164244b24f0204
Matti Nelimarkka <matti.nelimarkka@gmail.com>
2012-06-20 23:42:16 +0300

    Load stuff after all modules have been loaded

---
+++ plugins/finish.js
@@ -0,0 +1,24 @@
+/*global yepnope, jQuery */
+
+yepnope(
+    {
+        load: [
+            'lib/raphael-1.3.1-min.js',
+            'lib/raphael-path.js',
+            'lib/sketchy.js',
+            'lib/colorwheel.js',
+            'lib/beautify.js',
+            'lib/highlight.js',
+            'lib/highlight-javascript.js',
+            'lib/highlight-github.css'
+        ],
+        complete: setup
+    }
+);
+
+function setup(){
+
+    console.log('All done?');
+    $('#block_menu').accordion({ autoHeight: false });
+
+};
