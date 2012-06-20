5abee275aefeaafcd466113d01269cda3f8e9321
Matti Nelimarkka <matti.nelimarkka@gmail.com>
2012-06-20 17:59:06 +0300

    Search person with name like-block

--- plugins/fb.js
+++ plugins/fb.js
@@ -34,10 +34,14 @@ var menus = {
             script: '{{1}}.name',
             type: 'string'
 	      }, {
-            label: 'image of [object]',
+            label: 'image of [any]',
             script: '(function(){var img = new Image(); img.src="https://graph.facebook.com/" + {{1}}.id + "/picture"; return img;})',
             type: 'image'
-	      }
+	      } , {
+		label: 'friend with name like [string]',
+		script: '(function(){var correct = {id: "", name: ""}; $.each( fb.friends.data , function(i, user) { if( user.name.indexOf( {{1}} ) != -1 ) correct = user; } ); return correct;})()',
+		type: 'object'
+		}
     ])
 };
 
