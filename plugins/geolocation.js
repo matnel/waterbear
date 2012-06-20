fc950cb40b7a333f35195e6d92234991923cdeca
Matti Nelimarkka <matti.nelimarkka@gmail.com>
2012-06-20 18:54:02 +0300

    Block for current location & updating location

--- plugins/geolocation.js
+++ plugins/geolocation.js
@@ -9,14 +9,28 @@ yepnope(
 
 function setup() {};
 
+var loc = {};
+
 var menus = {
     geolocation : menu('Location', [
     {
     label: 'my location',
-    script: '{lat: 0, long: 0}',
+    script: 'loc',
     type: 'location'
+    } , {
+    label: 'update location',
+    script: 'navigator.geolocation.getCurrentPosition( function(data){ loc.latitude = data.coords.latitude; loc.longitude = data.coords.longitude; }, $.noop);'
     }
-}; 
+] ) }; 
+
+function location( data ) {
+  loc.latitude = data.coords.latitude;
+  loc.longitude = data.coords.longitude;
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(location, $.noop);
+}
 
 load_current_scripts();
 $('.scripts_workspace').trigger('init');
