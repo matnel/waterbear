7a36505f101e6734674c633f9f088636f252b501
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-06-26 23:33:16 +0300

    Code cleanup

--- plugins/accelerometer.js
+++ plugins/accelerometer.js
@@ -1,4 +1,4 @@
-/*global yepnope, FB */
+/*global yepnope, $, menu */
 yepnope(
     {
         load: ['plugins/javascript.css'],
@@ -6,7 +6,7 @@ yepnope(
     }
 );
 
-function setup() {};
+function setup(){};
 
 var accelerometer = {};
 accelerometer.direction = "";
@@ -16,63 +16,56 @@ accelerometer.DIR = 0;
 
 var menus = {
     accelerometer : menu('Motion', [
-    {
-    label: 'tilt direction',
-    script: 'accelerometer.direction',
-    type: 'string'
-    } , {
-    label: 'when device turned [choice:directions]', 
-    trigger: true,
-    slot: false,
-    containers: 1,
-    script: 'setInterval( function(){ if(accelerometer.direction.indexOf( {{1}} ) != -1 ){ [[1]] } }, 1000);'
+        {
+            label: 'tilt direction',
+            script: 'accelerometer.direction',
+            type: 'string'
+        }, {
+            label: 'when device turned [choice:directions]',
+            trigger: true,
+            slot: false,
+            containers: 1,
+            script: 'setInterval( function(){ if(accelerometer.direction.indexOf( {{1}} ) != -1 ){ [[1]] } }, 1000);'
         }
-      ])
+    ])
 }; 
 
 if(window.DeviceOrientationEvent){
-
-	window.addEventListener('deviceorientation', function(eventData) {
-	
-	//Otetaan kallistuskulmat talteen
-		accelerometer.LR = event.gamma;
-		accelerometer.FB = event.beta;
-		accelerometer.DIR = event.alpha;
-		
-		//Päivitetään laitteen asento
-		getTilt();
-		});
+	window.addEventListener('deviceorientation', processData );
 }else{
 	console.log("Ei toomi tässä :/");
 }
 	
-function getTilt(){
-		
-		//Raja, jossa kallistuskulma menee.
-		var limit = 10;
-		accelerometer.direction = "";
-		
-		//Väli-ilmansuunnat
-		if(accelerometer.FB > limit && accelerometer.LR > limit){
-			accelerometer.direction = "upright";
-		}else if(accelerometer.LR > limit && accelerometer.FB < -limit){
-			accelerometer.direction = "downright";
-		}else if(accelerometer.LR < -limit && accelerometer.FB < -limit){
-			accelerometer.direction = "downleft";
-		}else if(accelerometer.LR < -limit && accelerometer.FB > limit){
-			accelerometer.direction = "upright";
-		//Pääilmansuunnat
-		}else if(accelerometer.FB > limit){
-			accelerometer.direction = "up";
-		}else if(accelerometer.LR > limit){
-			accelerometer.direction = "right";
-		}else if(accelerometer.FB < -limit){
-			accelerometer.direction = "down";
-		}else if(accelerometer.LR < -limit){
-			accelerometerdirection =  "left";
-		}
-	
-		return accelerometer.direction;
+function processData( event ){
+
+
+        accelerometer.LR = event.gamma;
+        accelerometer.FB = event.beta;
+        accelerometer.DIR = event.alpha;
+
+        var limit = 10;
+        accelerometer.direction = "";
+        
+        //Väli-ilmansuunnat
+        if(accelerometer.FB > limit && accelerometer.LR > limit){
+            accelerometer.direction = "upright";
+        }else if(accelerometer.LR > limit && accelerometer.FB < -limit){
+            accelerometer.direction = "downright";
+        }else if(accelerometer.LR < -limit && accelerometer.FB < -limit){
+            accelerometer.direction = "downleft";
+        }else if(accelerometer.LR < -limit && accelerometer.FB > limit){
+            accelerometer.direction = "upright";
+        //Pääilmansuunnat
+        }else if(accelerometer.FB > limit){
+            accelerometer.direction = "up";
+        }else if(accelerometer.LR > limit){
+            accelerometer.direction = "right";
+        }else if(accelerometer.FB < -limit){
+            accelerometer.direction = "down";
+        }else if(accelerometer.LR < -limit){
+            accelerometer.direction =  "left";
+        }
+        
 };
 
 load_current_scripts();
